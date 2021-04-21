f6cc9ea5f3d8848c0ba8acb5d6a722cda13cca6a
Thiago Felipe <thiagof.g3@gmail.com>
2021-04-21 17:06:08 -0300

    Criando controller para salvar setting

---
+++ src/controllers/SettingController.ts
@@ -0,0 +1,24 @@
+import { Request , Response } from 'express';
+import { getCustomRepository } from "typeorm";
+import SettingRepository from "../repositories/SettingRepository";
+
+class SettingController {
+    async create(request: Request, response: Response) {
+        try {
+            const { chat, username } = request.body;
+            const settingRepository = getCustomRepository(SettingRepository);
+            const setting = settingRepository.create({
+                chat,
+                username
+            });
+
+            await settingRepository.save(setting);
+
+            return response.json(setting);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default SettingController;
---
+++ src/routers.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import SettingController from "./controllers/SettingController";
+
+
+const routes = Router();
+
+const settingController = new SettingController();
+
+routes.post('/settings', settingController.create);
+
+
+export default routes;
