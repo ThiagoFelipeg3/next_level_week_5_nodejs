4e43dfbfea375686745f93d95101675d46f95e13
Thiago Felipe <thiagof.g3@gmail.com>
2021-04-21 17:04:29 -0300

    Configuracoes iniciais

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import express from 'express';
+import routes from './routers';
+
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Se besta GET'
-    });
-});
-
-app.post('/user/:id', (req, response) => {
-    const { body: bodyRequest } = req;
-    console.log(bodyRequest);
-
-    return response.json({
-        message: `Se besta POST: ${bodyRequest}`
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
